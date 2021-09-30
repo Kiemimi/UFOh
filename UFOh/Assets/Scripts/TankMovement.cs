0261610c628c620362f18289a893b752fffcf5e4
May Broome <89263071+Kiemimi@users.noreply.github.com>
2021-09-30 18:24:55 +0100

    We have a level

--- UFOh/Assets/Scripts/TankMovement.cs
+++ UFOh/Assets/Scripts/TankMovement.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class TankMovement : MonoBehaviour
 {
-
     public float moveSpeed = 500f;
     public Rigidbody2D tankRigidBody;
     Vector3 getTrans;
--- UFOh/Assets/Scripts/UFOMove.cs
+++ UFOh/Assets/Scripts/UFOMove.cs
@@ -4,43 +4,41 @@ using UnityEngine;
 
 public class UFOMove : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-
+    public float moveSpeed = 500f;
     public Rigidbody2D UFORigidBody;
+    Vector3 getTrans;
 
-    Vector2 movement;
 
+    // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, 0, -120 * Time.deltaTime);
+        }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            movement.x = -1;
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            movement.x = 1;
-        }
-        else
-        {
-            movement.x = 0;
+            transform.Rotate(0, 0, 120 * Time.deltaTime);
         }
 
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow) == true)
         {
-            movement.y = -1;
+            getTrans = transform.up;
+            UFORigidBody.velocity = -getTrans * moveSpeed;
         }
-        else if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow) == true)
         {
-            movement.y = 1;
+            getTrans = transform.up;
+            UFORigidBody.velocity = getTrans * moveSpeed;
         }
-        else
-        {
-            movement.y = 0;
-        }
-    }
 
-    void FixedUpdate()
-    {
-        UFORigidBody.MovePosition(UFORigidBody.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            UFORigidBody.velocity = -(Vector2)transform.TransformDirection(Vector3.up) * 0;
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            UFORigidBody.velocity = (Vector2)transform.TransformDirection(Vector3.up) * 0;
+        }
     }
 }
