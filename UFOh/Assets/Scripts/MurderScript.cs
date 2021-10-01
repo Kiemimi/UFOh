dfc14009081177b5c91e0cebf00c6249ad381f58
May Broome <89263071+Kiemimi@users.noreply.github.com>
2021-10-01 01:27:25 +0100

    Screenshake and Soundscape

--- UFOh/Assets/Scripts/MurderScript.cs
+++ UFOh/Assets/Scripts/MurderScript.cs
@@ -6,19 +6,22 @@ public class MurderScript : MonoBehaviour
 {
     public int health = 60;
     public GameObject deathEffect;
+    public GameObject camera;
 
     public void TakeDamage (int damage)
     {
         health -= damage;
 
-        if(health <= 0)
+        if (health <= 0)
         {
+            
             Die();
         }
     }
 
     void Die()
     {
+        camera.GetComponent<screenShake>().TriggerShake();
         Instantiate(deathEffect, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
---
+++ UFOh/Assets/screenShake.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screenShake : MonoBehaviour
+{
+    private Transform transform;
+    private float shakeDuration = 0f;
+    private float shakeMagnitude = 0.45f;
+    private float dampingSpeed = 6.0f;
+    Vector3 initialPosition;
+
+    void Awake()
+    {
+        if (transform == null)
+        {
+            transform = GetComponent(typeof(Transform)) as Transform;
+        }
+    }
+
+    void OnEnable()
+    {
+        initialPosition = transform.localPosition;
+    }
+
+    void Update()
+    {
+        if (shakeDuration > 0)
+        {
+            transform.localPosition = initialPosition + Random.insideUnitSphere * shakeMagnitude;
+            shakeDuration -= Time.deltaTime * dampingSpeed;
+        }
+        else
+        {
+            shakeDuration = 0f;
+            transform.localPosition = initialPosition;
+        }
+    }
+    public void TriggerShake()
+    {
+        shakeDuration = 1.8f;
+    }
+}
